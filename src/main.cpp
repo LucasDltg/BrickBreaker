2d3d262ca50cc8b4ceede57aa52f3fc06f2f5350
Lucas Deletang <lucas.deletang.legravier@gmail.com>
2024-05-14 23:46:41 +0200

    fix textureManager

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,10 @@ int32_t main()
 
     SDLApp app(screen_width, screen_height, SDL_WINDOW_RESIZABLE, "../assets/fonts/arial/arial.ttf");
     app.addComponent(std::make_shared<BreakoutMenu>(), SDL_FRect{0.0f, 0.0f, 1.0f, 1.0f}, true);
-    // app.addComponent(std::make_shared<BreakoutMenu>(), SDL_FRect{0.0f, 0.0f, 1.0f, 1.0f}, false);
+    // app.addComponent(std::make_shared<BreakoutMenu>(), SDL_FRect{0.0f, 0.0f, 0.5f, 0.5f}, true);
+    // app.addComponent(std::make_shared<BreakoutMenu>(), SDL_FRect{0.5f, 0.0f, 1.0f, 0.5f}, true);
+    // app.addComponent(std::make_shared<BreakoutMenu>(), SDL_FRect{0.0f, 0.5f, 0.5f, 1.0f}, true);
+    // app.addComponent(std::make_shared<BreakoutMenu>(), SDL_FRect{0.5f, 0.5f, 1.0f, 1.0f}, true);
     
     app.run({}, 90);
 
